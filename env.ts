e495fc4d85215e716f81535964f026003e86afb1
aydrian <aydrian@gmail.com>
2023-11-19 11:37:29 -0500

    chore: not ready for env.ts yet

--- env.ts
+++ env.ts
@@ -1,28 +1,28 @@
-import { defineConfig } from "@julr/vite-plugin-validate-env";
-import { type TypeOf, z } from "zod";
+// import { defineConfig } from "@julr/vite-plugin-validate-env";
+// import { type TypeOf, z } from "zod";
 
-const schema = {
-  // Clerk configuration
-  CLERK_AFTER_SIGN_IN_URL: z.string(),
-  CLERK_AFTER_SIGN_UP_URL: z.string(),
-  CLERK_PUBLISHABLE_KEY: z.string(),
-  CLERK_SECRET_KEY: z.string(),
-  CLERK_SIGN_IN_URL: z.string(),
-  CLERK_SIGN_UP_URL: z.string(),
-  // Database configuration
-  DATABASE_PATH: z.string(),
-  DATABASE_URL: z.string()
-};
+// const schema = {
+//   // Clerk configuration
+//   CLERK_AFTER_SIGN_IN_URL: z.string(),
+//   CLERK_AFTER_SIGN_UP_URL: z.string(),
+//   CLERK_PUBLISHABLE_KEY: z.string(),
+//   CLERK_SECRET_KEY: z.string(),
+//   CLERK_SIGN_IN_URL: z.string(),
+//   CLERK_SIGN_UP_URL: z.string(),
+//   // Database configuration
+//   DATABASE_PATH: z.string(),
+//   DATABASE_URL: z.string()
+// };
 
-const zodEnv = z.object(schema);
+// const zodEnv = z.object(schema);
 
-export default defineConfig({
-  schema,
-  validator: "zod"
-});
+// export default defineConfig({
+//   schema,
+//   validator: "zod"
+// });
 
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv extends TypeOf<typeof zodEnv> {}
-  }
-}
+// declare global {
+//   namespace NodeJS {
+//     interface ProcessEnv extends TypeOf<typeof zodEnv> {}
+//   }
+// }
