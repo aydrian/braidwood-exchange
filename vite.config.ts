0193e332ba9880b7e096d94f7fd406aff7521799
aydrian <aydrian@gmail.com>
2023-11-19 11:49:45 -0500

    chore: disabling rdt

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
-import { remixDevTools } from "remix-development-tools/vite";
+// import { remixDevTools } from "remix-development-tools/vite";
 import { flatRoutes } from "remix-flat-routes";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -10,7 +10,7 @@ export default ({ mode }: { mode: string }) => {
   return defineConfig({
     optimizeDeps: { exclude: ["sharp"] },
     plugins: [
-      remixDevTools(),
+      // remixDevTools(),
       remix({
         ignoredRouteFiles: ["**/.*"],
         routes: async (defineRoutes) => {
